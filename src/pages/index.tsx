c11e9844cb688c5e027dd0bcfba7553031067603
Nikkolla <arebanikko@gmail.com>
2023-04-10 01:07:21 +0400

    slider corrected

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,8 @@ const { t } = useTranslation("common");
     <main  className="h-auto min-h-screen bg-black">
       <div className="pt-[65px] md:pt-[80px]">
       <Carousel withIndicators height={350} slideSize="80%" slideGap="md">
-      <Carousel.Slide><img  alt="nightclub tbilisi"  src="https://i.imgur.com/wpYjTDT.png"/></Carousel.Slide>
       <Carousel.Slide><img  alt="nightclub tbilisi"  src="https://i.imgur.com/jsBPE8i.png"/></Carousel.Slide>
+      <Carousel.Slide><img  alt="nightclub tbilisi"  src="https://i.imgur.com/wpYjTDT.png"/></Carousel.Slide>
       <Carousel.Slide><img  alt="nightclub tbilisi"  src="https://i.imgur.com/IM0tx8D.png"/></Carousel.Slide>
       <Carousel.Slide><img  alt="nightclub tbilisi"  src="https://i.imgur.com/RLBTuxk.png"/></Carousel.Slide>
     </Carousel>
