0fbcf0b4f8799e2bf10cbe682d9fc50529d01333
Nikkolla <arebanikko@gmail.com>
2023-04-10 00:44:15 +0400

    contact us pag

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,12 +1,24 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "react-i18next";
 
 export default function About() {
+  const { t } = useTranslation("common");
+  
     return (
-        <div>
-        <h1>About</h1>
+        <div className="h-auto min-h-screen text-center text-white bg-black md:pt-[80] pt-[50px] ">
+          <div className="text-xl py-14">
+            {t('contactus')}
+            </div>          
+            <div>
+              მაგიდის დასაჯავშნად დარეკეთ შემდეგ ნომერზე: 599 99 99 99 <br />
+              ან დააჭირეთ ამ ღილაკს <br />
+              ინფორმაციისთვის გადადით ჩვენს შესახებ გვერდზე ან დააჭირეთ ამ ღილაკს
+            </div>
+
+          
         </div>
     );
-    }
+  }
 
 
 
