7fe77c7ca1dceb279bc9dcab3c610fe1b9f187f4
Nikkolla <arebanikko@gmail.com>
2023-04-10 00:17:43 +0400

    5th update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { Carousel } from "@mantine/carousel";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"; 
+import Image from "next/image";
 import { useTranslation } from "react-i18next";
 
 
@@ -9,7 +11,14 @@ const { t } = useTranslation("common");
 
   
   return (
-    <main  className="h-auto min-h-screen bg-black pt-[80px]">
+    <main  className="h-auto min-h-screen bg-black">
+      <div className="pt-[50px] md:pt-[80px]">
+      <Carousel withIndicators height={350} slideSize="80%" slideGap="md">
+      <Carousel.Slide><Image alt="nightclub tbilisi" width={300} height={350} src="https://i.imgur.com/jsBPE8i.png"/></Carousel.Slide>
+      <Carousel.Slide><Image alt="nightclub tbilisi" width={300} height={350} src="https://i.imgur.com/IM0tx8D.png"/></Carousel.Slide>
+    </Carousel>
+
+      </div>
       <div className='text-[#e7d0ba]'>
         <p className="md:w-[60%] text-center mr-auto ml-auto py-20 text-xs">
           <div className="flex flex-row items-center justify-center pb-10 text-white">
