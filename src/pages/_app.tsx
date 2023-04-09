c8d386ba64ec00428b0fc380878fcaf4dc24d2c7
Nikkolla <arebanikko@gmail.com>
2023-04-10 00:32:54 +0400

    6th update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { appWithTranslation } from 'next-i18next';
 import type { AppType } from 'next/app'
 import { Notifications } from '@mantine/notifications';
 import { MantineProvider } from "@mantine/core";
+import { Footer } from '@/components/footer';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
@@ -12,6 +13,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     <MantineProvider withNormalizeCSS withGlobalStyles>
     <Notifications />
   <Component {...pageProps} />
+  <Footer />
     </MantineProvider>
     </div>
 
